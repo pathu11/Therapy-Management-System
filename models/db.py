de7cfb48a99e2745fdaf52d2e147506e0d2d3751
M.K.P.Ahinsa <pathuahinsa2001@gmail.com>
2024-12-27 07:58:21 +0530

    add changes

--- models/db.py
+++ models/db.py
@@ -1,6 +1,6 @@
 import sqlite3
-import os
-DATABASE = 'database/mental_health.db'
+
+DATABASE = '../mental_health.db'
 
 def get_db():
     conn = sqlite3.connect(DATABASE)
@@ -8,14 +8,6 @@ def get_db():
     return conn
 
 def init_db():
-    
-    if not os.path.exists(DATABASE):
-        os.makedirs(os.path.dirname(DATABASE), exist_ok=True)
-
-    os.chmod('database', 0o777)  
-    if os.path.exists(DATABASE):
-        os.chmod(DATABASE, 0o666) 
-        
     with open('database/setup_db.sql', 'r') as f:
         query = f.read()
     conn = get_db()
